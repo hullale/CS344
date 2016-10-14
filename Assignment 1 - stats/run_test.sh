4524151b859c14ccc7f08905a4f3bd6a0266e8fc
Corwin Perren <caperren@gmail.com>
2016-10-13 22:29:20 -0700

    Cleaned up and submitted...

--- Assignment 1 - stats/run_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sum=32
-count=6
-echo "(${sum}/${count})" | bc -l
-awk_proper_rounding='
-    {printf("%d\n",$1 + 0.5)}
-'
-
-echo "(${sum}/${count})" | bc -l | awk "${awk_proper_rounding}"
