a91791075868db5ffe2030e0947d04f6310deeca
Corwin Perren <caperren@gmail.com>
2016-11-30 22:12:11 -0800

    Keygen fully functional. Encoding/Decoding functions work.....

--- Assignment4-OTP/keygen.c
+++ Assignment4-OTP/keygen.c
@@ -7,22 +7,45 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-#define PROGRAM_SUCESS 0
+#define PROGRAM_SUCCESS 0
 #define PROGRAM_FAILURE 1
 
-/*
- * 
- */
+#define ASCII_START 65
+
 int main(int argc, char** argv) {
+    //Some error handling for bad inputs
     if(argc < 2){
-        fprintf(stderr, "No arguments entered.\nPlease try again.\n");
+        fprintf(stderr, "No arguments entered. Please try again.\n");
+        exit(PROGRAM_FAILURE);
+    }else if (argc > 2){
+        fprintf(stderr, \
+                "Extra arguments entered. Ignoring all but the first.\n");
+    }else if(atoi(argv[1]) <= 0){
+        fprintf(stderr, \
+                "Key length too small. Number must be greater than zero.\n");
         exit(PROGRAM_FAILURE);
     }
     
+    time_t t;
+    srand((unsigned) time(&t));
     
+    //Get the number passed in as an argument
+    int key_length = atoi(argv[1]);
     
+    //Loop through and output a new character based on the number generated
+    for(int i = 0 ; i < key_length ; i++){
+        int rand_char = rand() % 27;
+        if(rand_char == 26){
+            printf(" ");
+        }else{
+            printf("%c", (char)(rand_char + ASCII_START));
+        }
+    }
     
-    return (PROGRAM_SUCESS);
+    //Print the final newline and exit successfully.
+    printf("\n");
+    exit(PROGRAM_SUCCESS);
 }
 
--- Assignment4-OTP/otp_enc.c
+++ Assignment4-OTP/otp_enc.c
@@ -7,12 +7,102 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-/*
- * 
- */
+#define LETTER_OPTIONS 27
+int letter_number_assignment[LETTER_OPTIONS][2] = {
+    'A', 10,
+    'B', 11,
+    'C', 12,
+    'D', 13,
+    'E', 14,
+    'F', 15,
+    'G', 16,
+    'H', 17,
+    'I', 18,
+    'J', 19,
+    'K', 20,
+    'L', 21,
+    'M', 22,
+    'N', 23,
+    'O', 24,
+    'P', 25,
+    'Q', 26,
+    'R', 0,
+    'S', 1,
+    'T', 2,
+    'U', 3,
+    'V', 4,
+    'W', 5,
+    'X', 6,
+    'Y', 7,
+    'Z', 8,
+    ' ', 9
+};
+/////////////////////////////////////////
+////////// Function Prototypes //////////
+/////////////////////////////////////////
+int get_mapped_num_from_char(char letter);
+char get_mapped_char_from_num(int number);
+char encode_character(char letter, char key_letter);
+char decode_character(char input_letter, char key_letter);
+
+/////////////////////////////////////////
+////////// Function Prototypes //////////
+/////////////////////////////////////////
 int main(int argc, char** argv) {
-    printf("otp_enc");
-    return (EXIT_SUCCESS);
+    char *sentence = "MY OWN TEST";
+    
+    for(int i = 0 ; i < strlen(sentence) ; i++){
+        char encoded_char = encode_character(sentence[i], 'A' + i);
+        char decoded_char = decode_character(encoded_char, 'A' + i);
+        
+        printf("%c : %c : %c\n", sentence[i], encoded_char, decoded_char);
+    }
+    
+    exit (EXIT_SUCCESS);
 }
 
+int get_mapped_num_from_char(char letter){
+    for(int i = 0 ; i < LETTER_OPTIONS ; i++){
+        if(letter == letter_number_assignment[i][0]){
+            return letter_number_assignment[i][1];
+        }
+    }
+    return -1;
+}
+
+char get_mapped_char_from_num(int number){
+    for(int i = 0 ; i < LETTER_OPTIONS ; i++){
+        if(number == letter_number_assignment[i][1]){
+            return letter_number_assignment[i][0];
+        }
+    }
+    return 'e';
+}
+
+char encode_character(char input_letter, char key_letter){
+    int letter_mapped = get_mapped_num_from_char(input_letter);
+    int key_mapped = get_mapped_num_from_char(key_letter);
+    
+    int summed = (letter_mapped + key_mapped);
+    
+    if(summed > LETTER_OPTIONS){
+        summed -= LETTER_OPTIONS;
+    }
+    
+    return get_mapped_char_from_num(summed);
+}
+
+char decode_character(char input_letter, char key_letter){
+    int letter_mapped = get_mapped_num_from_char(input_letter);
+    int key_mapped = get_mapped_num_from_char(key_letter);
+    
+    int subbed = (letter_mapped - key_mapped);
+    
+    if(subbed < 0){
+        subbed += LETTER_OPTIONS;
+    }
+    
+    return get_mapped_char_from_num(subbed);
+}
